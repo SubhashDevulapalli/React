e8fed5e6d3906d89c3137c6eae486a30d75e4e68
Subhash_Devulapalli <subhashdevulapalli149@gmail.com>
2025-07-20 02:39:39 -0400

    Added parcel importance

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 // EC-1: Example Code for creating nested HTML tags in React
-// const parent = React.createElement(
-//     "div",
-//     { id: "parent" },
-//     [
-//         React.createElement(
-//             "div",
-//             { id: "child1" },
-//             React.createElement("h1", { id: "Child-1-h1" }, "This is h1 tag of child-1")
-//         ),
-//         React.createElement(
-//             "div",
-//             { id: "child2" },
-//             React.createElement("h1", { id: "Child-2-h1" }, "This is h1 tag of child-2")
-//         ),
-//         React.createElement("div", { id: "child3" }, "Child 3")
-//     ]
-// );
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    [
+        React.createElement(
+            "div",
+            { id: "child1" },
+            React.createElement("h1", { id: "Child-1-h1" }, "This is h1 tag of child-1")
+        ),
+        React.createElement(
+            "div",
+            { id: "child2" },
+            React.createElement("h1", { id: "Child-2-h1" }, "This is h1 tag of child-2")
+        ),
+        React.createElement("div", { id: "child3" }, "Child 3")
+    ]
+);
 
 
 const element = React.createElement(
@@ -29,3 +30,4 @@ console.log(element);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(element);
+// export default element;
